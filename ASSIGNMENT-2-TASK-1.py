9a7caee0910f3ef403570d4f1c949fa6ed7a1c2b
Muzammil Tanwar <itsmuzammiltanwar@gmail.com>
2025-05-07 16:44:11 +0530

    Add files via upload

---
+++ ASSIGNMENT-2-TASK-1.py
@@ -0,0 +1,5 @@
+number=int(input("Enter a number you wish to check: "))
+if number %2 == 0:
+    print ("\nThe number is even")
+else:
+    print ("\nThe number is odd")
---
+++ ASSIGNMENT-2-TASK-2.py
@@ -0,0 +1,4 @@
+counter=0
+for i in range(1,51):
+    counter += i
+print("\nThe sum of integers between 1 and 50 is: ", counter)
